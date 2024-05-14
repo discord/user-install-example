4fe83a954abd77c3c558b4d61ab55d4cf2a1c128
Cairnsy <96149457+alioop222@users.noreply.github.com>
2024-05-14 18:42:19 +0100

    modified:   commands.js

--- commands.js
+++ commands.js
@@ -47,11 +47,47 @@ const LINK_COMMAND = {
   contexts: [1],
 };
 
+// Scrape command to scrape conversation text
+const SCRAPE_COMMAND = {
+  name: 'scrape',
+  type: 1,
+  description: 'Scrape the full text of a conversation with another user',
+  options: [
+    {
+      type: 6,
+      name: 'user',
+      description: 'The user to scrape the conversation with',
+      required: true,
+    },
+  ],
+  integration_types: [1],
+  contexts: [0, 1, 2],
+};
+
+// Print command to print conversation text to a file
+const PRINT_COMMAND = {
+  name: 'print',
+  type: 1,
+  description: 'Print the scraped conversation text to a .txt file',
+  options: [
+    {
+      type: 3,
+      name: 'filename',
+      description: 'The name of the file to print the conversation to',
+      required: true,
+    },
+  ],
+  integration_types: [1],
+  contexts: [0, 1, 2],
+};
+
 const ALL_COMMANDS = [
   WIKI_COMMAND,
   LEADERBOARD_COMMAND,
   PROFILE_COMMAND,
   LINK_COMMAND,
+  SCRAPE_COMMAND,
+  PRINT_COMMAND,
 ];
 
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
