3cc8dfbf2f3bcc69c3981c3bb2d1f8c51b858db0
Cairnsy <96149457+alioop222@users.noreply.github.com>
2024-05-15 18:06:25 +0100

    new file:   API_KEY 	modified:   commands.js

--- commands.js
+++ commands.js
@@ -3,40 +3,40 @@ import { fakeGameItems } from './game.js';
 import { InstallGlobalCommands } from './utils.js';
 
 // Wiki command for game lookup
-const WIKI_COMMAND = {
-  name: 'wiki',
-  type: 1,
-  description: 'Lookup information in wiki',
-  options: [
-    {
-      type: 3,
-      name: 'item',
-      description: 'Item to lookup',
-      choices: fakeGameItems,
-      required: true,
-    },
-  ],
-  integration_types: [0, 1],
-  contexts: [0, 1, 2],
-};
+// const WIKI_COMMAND = {
+//   name: 'wiki',
+//   type: 1,
+//   description: 'Lookup information in wiki',
+//   options: [
+//     {
+//       type: 3,
+//       name: 'item',
+//       description: 'Item to lookup',
+//       choices: fakeGameItems,
+//       required: true,
+//     },
+//   ],
+//   integration_types: [0, 1],
+//   contexts: [0, 1, 2],
+// };
 
 // Leaderboard command, for guild install only
-const LEADERBOARD_COMMAND = {
-  name: 'leaderboard',
-  type: 1,
-  description: 'See server leaderboard',
-  integration_types: [0],
-  contexts: [0],
-};
+// const LEADERBOARD_COMMAND = {
+//   name: 'leaderboard',
+//   type: 1,
+//   description: 'See server leaderboard',
+//   integration_types: [0],
+//   contexts: [0],
+// };
 
 // Profile command
-const PROFILE_COMMAND = {
-  name: 'profile',
-  type: 1,
-  description: 'See your game inventory and progress',
-  integration_types: [1],
-  contexts: [0, 1, 2],
-};
+// const PROFILE_COMMAND = {
+//   name: 'profile',
+//   type: 1,
+//   description: 'See your game inventory and progress',
+//   integration_types: [1],
+//   contexts: [0, 1, 2],
+// };
 
 // Link account command
 const LINK_COMMAND = {
@@ -61,7 +61,7 @@ const SCRAPE_COMMAND = {
     },
   ],
   integration_types: [1],
-  contexts: [0, 1, 2],
+  contexts: [0],
 };
 
 // Print command to print conversation text to a file
@@ -78,13 +78,13 @@ const PRINT_COMMAND = {
     },
   ],
   integration_types: [1],
-  contexts: [0, 1, 2],
+  contexts: [0],
 };
 
 const ALL_COMMANDS = [
-  WIKI_COMMAND,
-  LEADERBOARD_COMMAND,
-  PROFILE_COMMAND,
+  // WIKI_COMMAND,
+  // LEADERBOARD_COMMAND,
+  // PROFILE_COMMAND,
   LINK_COMMAND,
   SCRAPE_COMMAND,
   PRINT_COMMAND,
